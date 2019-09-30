6f5c6d862c71a083033ce4307acaa76a3b43c259
chengjian <cj@sicnu.edu.cn>
2019-09-30 18:43:21 +0800

    update

---
+++ src/main/java/singleton/SquadLeader.java
@@ -0,0 +1,9 @@
+package singleton;
+
+/**
+ * 班长类
+ * @author chengjian
+ * @date 2019/9/29
+ */
+public class SquadLeader {
+}
---
+++ src/main/java/singleton/XyzClass.java
@@ -0,0 +1,9 @@
+package singleton;
+
+/**
+ * xyz班级
+ * @author chengjian
+ * @date 2019/9/29
+ */
+public class XyzClass {
+}
